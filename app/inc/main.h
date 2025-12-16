57cbc70731230a3a9a392ccc292e0ed323bdb9e4
Yago <yagompassos@gmail.com>
2025-12-16 18:04:57 +0100

    correct includes

--- app/inc/main.h
+++ app/inc/main.h
@@ -1,14 +1,26 @@
 /*
  * main.h
  *
- *  Created on: 16 août 2018
+ *  Created on: 16 aoï¿½t 2018
  *      Author: Laurent
  */
 
 #ifndef APP_INC_MAIN_H_
 #define APP_INC_MAIN_H_
 
+// Device header
 #include "stm32f0xx.h"
+// BSP functions
+#include "bsp.h"
+// FreeRTOS headers
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "semphr.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "stream_buffer.h"
 
 
 /* Global functions */
