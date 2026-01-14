f1619b31feb02797f446193312d66a0a12d66bd0
Yago <yagompassos@gmail.com>
2026-01-14 20:53:18 +0100

    separating the code in modules

---
+++ app/inc/blocker.h
@@ -0,0 +1,19 @@
+/*
+ * blocker.h
+ *
+ *  Created on: Jan 14, 2026
+ *      Author: Yago
+ */
+
+#ifndef INC_BLOCKER_H_
+#define INC_BLOCKER_H_
+
+
+/*
+ *	TaskControlBlocker controls "2. BLOCAGE ENTREE PALETTISEUR"
+ *	It's the barrier that let the packages in to one spot before the elevator
+ *
+ */
+void vTaskControlBlocker (void *pvParameters);
+
+#endif /* INC_BLOCKER_H_ */
---
+++ app/inc/distributor.h
@@ -0,0 +1,17 @@
+/*
+ * distributor.h
+ *
+ *  Created on: Jan 14, 2026
+ *      Author: Yago
+ */
+
+#ifndef INC_DISTRIBUTOR_H_
+#define INC_DISTRIBUTOR_H_
+
+
+/*
+ * Click at the blue button at your board, this generates new packages (cartons) to the scene.
+ */
+void vTaskDistribuitionCardBoards (void *pvParameters);
+
+#endif /* INC_DISTRIBUTOR_H_ */
